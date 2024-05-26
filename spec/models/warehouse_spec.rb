d49f99c773ea21550b75db9615d4994d0a76c6f5
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-26 06:29:06 -0300

    Add validate on Model test

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  it 'field cannot be blank' do
+    warehouse = Warehouse.new
+
+    warehouse.valid?
+
+    expect(warehouse.errors.full_messages).to include 'Nome não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'Código não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'Cidade não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'Descrição não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'Endereço não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'CEP não pode ficar em branco'
+    expect(warehouse.errors.full_messages).to include 'Área não pode ficar em branco'
+  end
+
+  it 'warehouse code needs to be unique' do
+    Warehouse.create!(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '25000-000', city: 'Rio',
+                      area: 1000, description: 'Alguma descrição')
+
+    warehouse = Warehouse.new(code: 'RIO')
+
+    warehouse.valid?
+
+    expect(warehouse.errors.full_messages).to include 'Código já está em uso'
+    expect(warehouse.errors.full_messages).not_to include 'Código não pode ficar em branco'
+  end
 end
